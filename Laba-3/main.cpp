f7a81b820a267d992a05ea17ff8d8fa338ff72a5
Taya Medvedeva <71186576+MTasia@users.noreply.github.com>
2020-11-19 14:30:56 +0300

    Update main.cpp

--- Laba-3/main.cpp
+++ Laba-3/main.cpp
@@ -1,1 +1,23 @@
+#include"menu.h"
+#include <iostream>
+#include <fstream>
 
+
+int mode(int argc, char** argv){
+	if(argc == 0)
+		return 1;
+	else
+		return 2;
+}
+
+
+int main(int argc, char** argv) {
+	string str;
+	ifstream in("menu.txt");
+	getline(in, str);
+	in.close();
+	
+	launch_menu(mode(argc, argv), str);
+
+	return 0;
+}
